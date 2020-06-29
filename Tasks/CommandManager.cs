8e0cfe2fe8b2f8cef7ca86651084ae65d59069ee
unknown <vianney.chepeau@epsi.fr>
2020-06-29 15:04:21 +0200

    ajout de commandManager permettant de regrouper toutes les différentes commandes

---
+++ Tasks/CommandManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    public class CommandManager
+    {
+        public ShowCommand show { get; }
+        
+        public CommandManager()
+        {
+            show = new ShowCommand();
+        }
+    }
+}
