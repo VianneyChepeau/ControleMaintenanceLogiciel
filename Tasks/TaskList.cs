94ee837740ea781609bf9151773cfd73a6f38894
unknown <vianney.chepeau@epsi.fr>
2020-06-29 16:55:00 +0200

    suppression de l'ancien code commenté + optimisation de la commande quitter

--- Tasks/TaskList.cs
+++ Tasks/TaskList.cs
@@ -5,14 +5,10 @@ using System.Linq;
 namespace Tasks
 {
 	public sealed class TaskList
-	{
-		private const string QUIT = "quit";
-
+	{ 
 		private IDictionary<string, IList<Task>> tasks = new Dictionary<string, IList<Task>>();
 		private static IConsole console;
 		private CommandManager commandManager;
-
-		//private long lastId = 0;
 
 		public static void Main(string[] args)
 		{
@@ -28,12 +24,8 @@ namespace Tasks
 		public void Run()
 		{
 			while (true) {
-				console.Write("> ");
-				var command = console.ReadLine();
-				if (command == QUIT) {
-					break;
-				}
-				Execute(command);
+				console.Write("> "); 
+				Execute(console.ReadLine());
 			}
 		}
 
@@ -59,94 +51,13 @@ namespace Tasks
 			case "help":
 				commandManager.help.Execute();
 				break;
+			case "quit":
+				Environment.Exit(0);
+				break;
 			default:
 				commandManager.error.Execute(command);
 				break;
 			}
 		}
-
-		//private void Show()
-		//{
-		//	foreach (var project in tasks) {
-		//		console.WriteLine(project.Key);
-		//		foreach (var task in project.Value) {
-		//			console.WriteLine("    [{0}] {1}: {2}", (task.Done ? 'x' : ' '), task.Id, task.Description);
-		//		}
-		//		console.WriteLine();
-		//	}
-		//}
-
-		//private void Add(string commandLine)
-		//{
-		//	var subcommandRest = commandLine.Split(" ".ToCharArray(), 2);
-		//	var subcommand = subcommandRest[0];
-		//	if (subcommand == "project") {
-		//		AddProject(subcommandRest[1]);
-		//	} else if (subcommand == "task") {
-		//		var projectTask = subcommandRest[1].Split(" ".ToCharArray(), 2);
-		//		AddTask(projectTask[0], projectTask[1]);
-		//	}
-		//}
-
-		//private void AddProject(string name)
-		//{
-		//	tasks[name] = new List<Task>();
-		//}
-
-		//private void AddTask(string project, string description)
-		//{
-		//	if (!tasks.TryGetValue(project, out IList<Task> projectTasks))
-		//	{
-		//		Console.WriteLine("Could not find a project with the name \"{0}\".", project);
-		//		return;
-		//	}
-		//	projectTasks.Add(new Task { Id = NextId(), Description = description, Done = false });
-		//}
-
-		//private void Check(string idString)
-		//{
-		//	SetDone(idString, true);
-		//}
-
-		//private void Uncheck(string idString)
-		//{
-		//	SetDone(idString, false);
-		//}
-
-		//private void SetDone(string idString, bool done)
-		//{
-		//	int id = int.Parse(idString);
-		//	var identifiedTask = tasks
-		//		.Select(project => project.Value.FirstOrDefault(task => task.Id == id))
-		//		.Where(task => task != null)
-		//		.FirstOrDefault();
-		//	if (identifiedTask == null) {
-		//		console.WriteLine("Could not find a task with an ID of {0}.", id);
-		//		return;
-		//	}
-
-		//	identifiedTask.Done = done;
-		//}
-
-		//private void Help()
-		//{
-		//	console.WriteLine("Commands:");
-		//	console.WriteLine("  show");
-		//	console.WriteLine("  add project <project name>");
-		//	console.WriteLine("  add task <project name> <task description>");
-		//	console.WriteLine("  check <task ID>");
-		//	console.WriteLine("  uncheck <task ID>");
-		//	console.WriteLine();
-		//}
-
-		//private void Error(string command)
-		//{
-		//	console.WriteLine("I don't know what the command \"{0}\" is.", command);
-		//}
-
-		//private long NextId()
-		//{
-		//	return ++lastId;
-		//}
 	}
 }
