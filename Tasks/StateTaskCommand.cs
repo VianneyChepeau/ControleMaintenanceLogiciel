271463e12eac3f7cd3d9ebaa6d5ffde8964c9568
unknown <vianney.chepeau@epsi.fr>
2020-06-29 16:18:16 +0200

    ajout StateTaskCommand + suppression CheckCommand car StateTaskCommand va englober CheckCommand et UncheckCommand

---
+++ Tasks/StateTaskCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    public class StateTaskCommand : ICommand
+    {
+        private readonly IConsole console;
+        private string idString;
+        private bool done;
+
+        public StateTaskCommand(IConsole console, string idString, bool done)
+        {
+            this.console = console;
+            this.idString = idString;
+            this.done = done;
+        }
+
+        public string GetName()
+        {
+            return done ? "check" : "uncheck";
+        }
+
+        public void Execute()
+        {
+
+        }
+    }
+}
