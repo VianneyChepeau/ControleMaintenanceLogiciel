ce58ca8139658dd6180a1574c2deaf4e61dae384
unknown <vianney.chepeau@epsi.fr>
2020-06-29 14:26:46 +0200

    modification du projet pour n'avoir que la partie cSharp dans le repot

---
+++ Tasks/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    public interface ICommand
+    {
+        string GetName();
+
+        void Execute();
+    }
+}
